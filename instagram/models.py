0181ddca327125275f00704d3e90e073512f9882
ow-tech <wagwanwekon@outloo.com>
2020-05-28 18:20:31 +0300

    registering of post model

--- instagram/models.py
+++ instagram/models.py
@@ -3,8 +3,11 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
-class post(models.Model):
+class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to="images/", null=False)
     caption = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.author, self.caption
