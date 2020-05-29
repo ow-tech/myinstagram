d74d3e7760d4cfb81700057ffbe2d1bc13622dbd
ow-tech <wagwanwekon@outloo.com>
2020-05-29 19:36:34 +0300

    creation of two-way authentication url_patterns;registration template and base for accounts

--- myinstagram/settings.py
+++ myinstagram/settings.py
@@ -138,3 +138,6 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
+
+# Account_Activation_DAYS
+Account_Activation_DAYS = 7 # 7 Days activation window
