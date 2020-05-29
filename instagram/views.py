42fddfddfafcc89eccb1edd8ecaf1e9bfaac45f0
ow-tech <wagwanwekon@outloo.com>
2020-05-29 19:40:18 +0300

    creatio of a single_post view

--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.contrib.auth.decorators import login_required
 
 
 
@@ -10,3 +11,11 @@ def main(request):
         'posts': Post.objects.all()
     }
     return render(request, 'instagram/main.html', context)
+
+@login_required(login_url='/accounts/login')
+def single_post(request):
+
+    context ={
+
+    }
+    return render(request, 'instagram/single_post.html', context)
