d58225fb95b8e70ae660515419f0b5150d5c00c2
ow-tech <wagwanwekon@outloo.com>
2020-06-03 11:03:08 +0300

    changing of urls

--- users/urls.py
+++ users/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('register/', user_views.register, name ='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('accounts/register/', user_views.register, name ='register'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path(r'profile/', user_views.profile, name = 'profile'),
 ] 
